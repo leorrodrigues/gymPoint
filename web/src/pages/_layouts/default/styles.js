f23b20755f61231a6f74d14007b5a4c86905075b
leonardo <leu_rr1@hotmail.com>
2020-01-02 21:45:49 -0300

    cleaning

--- web/src/pages/_layouts/default/styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Wrapper = styled.div`
-	height: 100%;
-	background: #f2f2f2;
-`;
--- web/src/store/modules/nav/actions.js
+++
@@ -1,6 +0,0 @@
-export function updateActiveNavigation(navItem) {
-	return {
-		type: '@nav/UPDATE_ACTIVE_NAVIGATION_REQUEST',
-		payload: { navItem },
-	};
-}
